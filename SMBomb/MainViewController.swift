4d34b663b483d8ee223fbb827c13a90b679127f8
Alexander Altman <qewhouse@gmail.com>
2023-08-06 21:23:17 +0300

    initial commit

--- SMBomb/MainViewController.swift
+++ SMBomb/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  SMBomb
 //
 //  Created by Alexander Altman on 06.08.2023.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .yellow
+        
     }
-
-
 }
-
