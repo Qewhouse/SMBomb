3608ed1391bdae980421b5fe820adaea977d66fa
Alexander Altman <qewhouse@gmail.com>
2023-08-06 21:11:44 +0300

    Initial Commit

---
+++ SMBomb/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SMBomb
+//
+//  Created by Alexander Altman on 06.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
