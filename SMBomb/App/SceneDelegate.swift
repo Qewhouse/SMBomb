fca5338dc34fa0a8ff682ef5ef4f1c4d90dc2c9c
Alexander Altman <qewhouse@gmail.com>
2023-08-07 20:25:29 +0300

    All prep work finalized
    + all assets integrated
    + all colours added to Theme enum file
    + LaunchScreen done

--- SMBomb/App/SceneDelegate.swift
+++ SMBomb/App/SceneDelegate.swift
@@ -15,6 +15,16 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
         let viewController = MainViewController() // изначальная настройка viewController
+        
+        /* Для того, чтобы автоматически отображать Ваш экран при запуске симулятора нужно:
+            1. Скопировать строку №17 на строку №18
+            2. В строке №18 поменять MainViewController на название Вашего ViewController
+            3. При завершении работы и перед Pull Request'ом удалить вашу строку №18 и раскоментировать строку №17
+         
+         - Для чего?
+         - Для того, чтобы избавиться от не нужных конфликтов при merge, ибо все дополнительные строки будут давать возможные конфликты
+         */
+        
         let navigationController = UINavigationController(rootViewController: viewController)
         window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
--- SMBomb/Resources/Theme.swift
+++ SMBomb/Resources/Theme.swift
@@ -10,9 +10,13 @@ import UIKit
 enum Theme {
     //MARK: Colors
     static let accentColor: UIColor = UIColor(named: "AccentColor")!
+    static let blackFont: UIColor = UIColor(named: "BlackFont")!
+    static let grayFont: UIColor = UIColor(named: "GrayFont")!
     static let orangeFont: UIColor = UIColor(named: "OrangeFont")!
-    
-    
+    static let violetBack: UIColor = UIColor(named: "VioletBack")!
+    static let violetFont: UIColor = UIColor(named: "VioletFont")!
+    static let yellowFont: UIColor = UIColor(named: "YellowFont")!
+
     //MARK: Font
     static let appFont: String = "DelaGothicOne-Regular"
 }
