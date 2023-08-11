26e4dc49e164cc61ba01ff30e218694c8c7c3d3a
Mirasque <obscure1321@gmail.com>
2023-08-11 14:09:33 +0300

    categories is finished

---
+++ SMBomb/Model/CategoryFlag.swift
@@ -0,0 +1,17 @@
+//
+//  CategoryFlag.swift
+//  SMBomb
+//
+//  Created by Miras Maratov on 11.08.2023.
+//
+
+import Foundation
+
+struct CategoryFlag {
+    static var isSelectedMisc = false
+    static var isSelectedHobby = false
+    static var isSelectedLife = false
+    static var isSelectedCelebs = false
+    static var isSelectedArt = false
+    static var isSelectedNature = false
+}
